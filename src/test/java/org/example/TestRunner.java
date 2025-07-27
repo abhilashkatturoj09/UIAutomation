3894ff30e7ea7e89aec0ec3e98d66e55238c6cdf
Abhilash Katturoj <abhilashkatturoj@Abhilashs-MacBook-Air.local>
2025-07-27 19:05:05 +0530

    added testRunner File

---
+++ src/test/java/org/example/TestRunner.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = {},
+        plugin = {"pretty", "html:target/cucumber-reports/cucumber-html-report.html"},
+        monochrome = true,
+        tags = "@"
+)
+public class TestRunner {
+}
